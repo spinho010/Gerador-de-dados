2df2bc8948d572f2b535230cd01575e197cbcb02
LucasOliver <78925489+spinho010@users.noreply.github.com>
2021-02-26 13:57:09 -0300

    Programa Finalizado com Sucesso!
    Comentarios sobre o programa, e finalizções

--- programa_principal.py
+++ programa_principal.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from random import randint
 
+#função para gerar os dados
 def generate():
     try:
         n1 = (randint(100, 999))
@@ -16,26 +17,34 @@ def generate():
     except:
         print('HOUVE UM ERRO!')
     else:
-        cpf1 = Label(janela, text=f'{n1}.{n2}.{n3}-{n4}', font='Arial 13', background='#fff', foreground='black')
-        cpf1.place(x=58, y=40)
+        cpf1 = Label(janela, text=f'{n1}.{n2}.{n3}-{n4}', font='Arial 18', background='#fff', foreground='black')
+        cpf1.place(x=70, y=40)
 
-        rg1 = Label(janela, text=f'{n5}.{n6}.{n7}-{n8}', font='Arial 13', background='#fff', foreground='black')
-        rg1.place(x=58, y=85)
+        rg1 = Label(janela, text=f'{n5}.{n6}.{n7}-{n8}', font='Arial 18', background='#fff', foreground='black')
+        rg1.place(x=70, y=85)
 
 
-
+#configurações da jenala
 janela = Tk()
 janela.geometry('400x500')
 janela.title('GERADOR DE DADOS')
 janela.configure(background='#008')
+#configurações da jenala
 
-txt1 = Label(janela, text='CPF: ', font='Arial 13', background='#fff', foreground='black')
+#Primeira escrita na Tela
+txt1 = Label(janela, text='CPF:.. ', font='Arial 18', background='#fff', foreground='black')
 txt1.place(x=15, y=40)
+#Primeira escrita na Tela
 
-txt2 = Label(janela, text='RG: ', font='Arial 13', background='#fff', foreground='black')
+#Segunda escrita na Tela
+txt2 = Label(janela, text='RG:.. ', font='Arial 18', background='#fff', foreground='black')
 txt2.place(x=15, y=85)
+#Segunda escrita na Tela
 
-botao = Button(janela, text='GERAR',command=generate, font='Bold 10', background='#fff', foreground='#008', width=18)
+#Botão que vai acionar a def ou função
+botao = Button(janela, text='GERAR',command=generate, font='Bold 15', background='#fff', foreground='#008', width=18)
 botao.place(x=15, y=150)
 
+#loop para janela permanecer aberta
 janela.mainloop()
+#fim

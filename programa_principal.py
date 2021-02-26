a0ca197612fce092c11811847b9abfacdce337e2
LucasOliver <78925489+spinho010@users.noreply.github.com>
2021-02-26 13:48:34 -0300

    modulo rg adicionado com sucesso!
    ....

--- programa_principal.py
+++ programa_principal.py
@@ -1,6 +1,5 @@
 from tkinter import *
-from defs import *
-import os
+from random import randint
 
 def generate():
     try:
@@ -8,19 +7,27 @@ def generate():
         n2 = (randint(100, 999))
         n3 = (randint(100, 999))
         n4 = (randint(10, 99))
+        n5 = (randint(10, 99))
+        n6 = (randint(100, 999))
+        n7 = (randint(100, 999))
+        n8 = (randint(1, 9))
         print(f'CPF GERADO COM SUCESSO! {n1}.{n2}.{n3}-{n4}')
+        print(f'RG GERADO COM SUCESSO! {n5}.{n6}.{n7}.{n8}')
     except:
         print('HOUVE UM ERRO!')
     else:
         cpf1 = Label(janela, text=f'{n1}.{n2}.{n3}-{n4}', font='Arial 13', background='#fff', foreground='black')
         cpf1.place(x=58, y=40)
 
+        rg1 = Label(janela, text=f'{n5}.{n6}.{n7}-{n8}', font='Arial 13', background='#fff', foreground='black')
+        rg1.place(x=58, y=85)
+
 
 
 janela = Tk()
 janela.geometry('400x500')
 janela.title('GERADOR DE DADOS')
-janela.configure(background='#fff')
+janela.configure(background='#008')
 
 txt1 = Label(janela, text='CPF: ', font='Arial 13', background='#fff', foreground='black')
 txt1.place(x=15, y=40)

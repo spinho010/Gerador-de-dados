4f993905604bbec983b91a0b1150e0ebccf3e9b9
LucasOliver <78925489+spinho010@users.noreply.github.com>
2021-02-26 13:00:30 -0300

    alterações simples
    .

--- programa_principal.py
+++ programa_principal.py
@@ -4,12 +4,15 @@ import os
 janela = Tk()
 janela.geometry('400x500')
 janela.title('GERADOR DE DADOS')
-janela.configure(background='#001')
+janela.configure(background='#fff')
 
-txt1 = Label(janela, text='CPF: ', font='Bold 13', background='#001', foreground='#fff')
+txt1 = Label(janela, text='CPF: ', font='Arial 13', background='#fff', foreground='black')
 txt1.place(x=15, y=40)
 
-txt2 = Label(janela, text='RG: ', font='Bold 13', background='#001', foreground='#fff')
+txt2 = Label(janela, text='RG: ', font='Arial 13', background='#fff', foreground='black')
 txt2.place(x=15, y=85)
+
+botao = Button(janela, text='GERAR', font='Bold 10', background='#fff', foreground='#008', width=18)
+botao.place(x=15, y=150)
 
 janela.mainloop()
